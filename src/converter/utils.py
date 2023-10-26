4ea47ac04dbf984b3f328078998320817f31b83b
PigeonsHouse <bullbull1230@gmail.com>
2023-10-26 20:48:24 +0900

    update: transparent_processの取得関数をアップデート

--- src/converter/utils.py
+++ src/converter/utils.py
@@ -1,17 +1,20 @@
 import ffmpeg
 
-origin_transparent_process = None
+origin_transparent_processes = {}
 
 
 def get_transparent_process(
     resolution_text: str,
 ):
     global origin_transparent_process
+    origin_transparent_process = origin_transparent_processes.get(
+        resolution_text
+    )
     if origin_transparent_process is None:
         origin_transparent_process = ffmpeg.input(
             "rgbtestsrc=s={}".format(resolution_text),
             f="lavfi",
         ).filter("geq", a=0, r=0, g=0, b=0)
-    transparent_process = origin_transparent_process.split()
-    origin_transparent_process = transparent_process[1]
-    return transparent_process[0]
+    transparent_processes = origin_transparent_process.sprit()
+    origin_transparent_processes[resolution_text] = transparent_processes[1]
+    return transparent_processes[0]
