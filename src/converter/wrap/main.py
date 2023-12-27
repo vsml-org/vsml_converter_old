4b584ae5ee5988d799e7b6ca1f7c5a11edf81408
PigeonsHouse <bullbull1230@gmail.com>
2023-12-27 16:31:08 +0900

    fix: style_to_filterの処理を使用するよう修正

--- src/converter/wrap/main.py
+++ src/converter/wrap/main.py
@@ -1,9 +1,11 @@
-import ffmpeg
-
 from content import WrapContent
 from converter.schemas import Process
+from converter.style_to_filter import (
+    get_background_color_code,
+    time_padding_end_filter,
+    time_padding_start_filter,
+)
 from style import Order, TimeUnit
-from utils import VSMLManager
 
 from .parallel import create_parallel_process
 from .sequence import create_sequence_process
@@ -14,8 +16,6 @@ def create_wrap_process(
     vsml_content: WrapContent,
     debug_mode: bool = False,
 ) -> Process:
-    fps = VSMLManager.get_root_fps()
-    style = vsml_content.style
     match vsml_content.style.order:
         case Order.SEQUENCE:
             process = create_sequence_process(
@@ -28,57 +28,20 @@ def create_wrap_process(
         case _:
             raise Exception()
 
-    background_color = (
-        style.background_color.value
-        if style.background_color
-        else "0x00000000"
+    style = vsml_content.style
+    background_color_code = get_background_color_code(style.background_color)
+    process.video, process.audio = time_padding_start_filter(
+        style.time_padding_start,
+        background_color_code,
+        process.video,
+        process.audio,
     )
-    if style.time_padding_start.unit in [TimeUnit.FRAME, TimeUnit.SECOND]:
-        video_option = {}
-        delays = int(style.time_padding_start.get_second(fps) * 1000)
-        if style.time_padding_start.unit == TimeUnit.FRAME:
-            video_option = {"start": style.time_padding_start.value}
-        elif style.time_padding_start.unit == TimeUnit.SECOND:
-            video_option = {
-                "start_duration": style.time_padding_start.get_second(fps),
-            }
-        if process.video is not None:
-            process.video = ffmpeg.filter(
-                process.video, "tpad", color=background_color, **video_option
-            )
-        if process.audio is not None:
-            process.audio = ffmpeg.filter(
-                process.audio,
-                "adelay",
-                all=1,
-                delays=delays,
-            )
-    if style.object_length.unit in [
-        TimeUnit.FRAME,
-        TimeUnit.SECOND,
-    ] and style.time_padding_end.unit in [TimeUnit.FRAME, TimeUnit.SECOND]:
-        video_option = {}
-        audio_option = {}
-        if style.time_padding_end.unit == TimeUnit.FRAME:
-            video_option = {
-                "stop": style.time_padding_end.value,
-            }
-            audio_option = {
-                "pad_dur": style.time_padding_end.get_second(fps),
-            }
-        elif style.time_padding_end.unit == TimeUnit.SECOND:
-            video_option = {
-                "stop_duration": style.time_padding_end.get_second(fps),
-            }
-            audio_option = {
-                "pad_dur": style.time_padding_end.get_second(fps),
-            }
-        if process.video is not None:
-            process.video = ffmpeg.filter(
-                process.video, "tpad", color=background_color, **video_option
-            )
-        if process.audio is not None:
-            process.audio = ffmpeg.filter(
-                process.audio, "apad", **audio_option
-            )
+    if style.object_length.unit in [TimeUnit.FRAME, TimeUnit.SECOND]:
+        process.video, process.audio = time_padding_end_filter(
+            style.time_padding_end,
+            background_color_code,
+            process.video,
+            process.audio,
+        )
+
     return process
